785c4c022c237d5a86140aba768931d89dfc4c67
Colin A A <98823661+corr6@users.noreply.github.com>
2022-03-07 19:53:48 +0000

    Completed lab

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import turtle
-
+import math
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
-
-
-
-
-
-
-
-
+def setupWindow(myWindow = None):
+  myWindow.reset()
+  myWindow.setworldcoordinates(-360,-1,360,1)
+def setupAxis(dart = None):
+  dart.up()
+  dart.goto(-360,0)
+  dart.down()
+  dart.goto(360,0)
+  dart.up()
+  dart.goto(0,-360)
+  dart.down()
+  dart.goto(0,360)
+def drawSineCurve(dart = None):
+  dart.up()
+  dart.goto(-360,0)
+  dart.color('blue')
+  dart.down()
+  for i in range(-360,361):
+    y=math.sin(math.radians(i))
+    dart.goto(i,y)
+def drawCosineCurve(dart = None):
+  dart.up()
+  dart.goto(-360,1)
+  dart.color('red')
+  dart.down()
+  for i in range(-360,361):
+    y=math.cos(math.radians(i))
+    dart.goto(i,y)
+def drawTangentCurve(dart = None):
+  dart.up()
+  dart.goto(-360,0)
+  dart.color('green')
+  dart.down()
+  for i in range(-360,361):
+    y=math.tan(math.radians(i))
+    dart.goto(i,y)
 ##########  Do Not Alter Any Code Past Here ########
 def main():
     #Part A
